249eead4c9f358a4009f402e2651cecd26588ebe
somerandomnoob1 <81366289+somerandomnoob1@users.noreply.github.com>
2021-03-25 08:43:39 -0400

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionEvent;
+	
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		GUI test = new GUI(280,280);
+
+		test.setUpBounds();
+		test.setUpButtonListeners();
+		test.setUpGUI();		
+	}
+	
+		
+}
